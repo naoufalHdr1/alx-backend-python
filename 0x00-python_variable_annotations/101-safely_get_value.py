f138dc1b97591c278494c614223fdc4cf42a35a4
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-10-10 19:30:29 +0100

    Task 11: fix check 3 error

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,12 +7,11 @@ from typing import Mapping, Any, TypeVar, Union
 
 T = TypeVar('T')
 
-
 def safely_get_value(
         dct: Mapping,
         key: Any,
         default: Union[T, None] = None
-        ) -> Union[T, None]:
+        ) -> Union[Any, T]:
     """
     Safely retrieves the value from a dictionary for the given key.
     """
