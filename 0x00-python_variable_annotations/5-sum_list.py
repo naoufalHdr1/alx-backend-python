f8d9db1b747f3e4ef0544561c050822e3a7eaeb6
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-10-10 18:24:37 +0100

    Task 5 - 6: import the type from the 'typing' module

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """
 This module contains a function to sum a list of floats
 """
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 """
-
+This module contains a function to sum a mixed list of integers and floats.
 """
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Calculate the sum of a mixed list of integers and floats.
+
+    Parameters:
+    mxd_lst (List[Union[int, float]]): A list of integers and floats
+    to be summed.
+
+    Returns:
+    float: The sum of the integers and floats in the list.
+    """
     return sum(mxd_lst)
