be5af658d86b241ad4aac6a28b82c901c2d9fb95
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-10-10 18:00:59 +0100

    Task 1: Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains a function to concatenate two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    Parameters:
+    str1 (str): The first string to concatenate.
+    str2 (str): The second string to concatenate.
+
+    Returns:
+    str: The concatenated string.
+    """
+    return str1 + str2
