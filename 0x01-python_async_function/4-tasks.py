cbe19383cc1d57fa3b64f84b0c153f500ca62623
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-10-14 22:39:50 +0100

    Task 4: forget a '->' at the function definition

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,7 +10,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawn task_wait_random n times with the specified max_delay.
     Return the list of all the delays in ascending order.
